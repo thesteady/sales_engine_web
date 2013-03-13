b6500f5838a93e8ef70f8f97605f71371dc737d1
Laura Steadman <lauramsteadman@gmail.com>
2013-03-13 08:43:58 -0600

    starting invoice items finders

--- lib/sales_engine_web/models/invoice_item.rb
+++ lib/sales_engine_web/models/invoice_item.rb
@@ -10,70 +10,50 @@ module SalesEngineWeb
       @quantity = params[:quantity]
     end
 
-#     def self.create(params)
-#       Item.new(params).save
-#     end
+    def self.create(params)
+      InvoiceItem.new(params).save
+    end
 
-#     def save
-#       @id = Item.add(self)
-#       self
-#     end
+    def save
+      @id = InvoiceItem.add(self)
+      self
+    end
 
-#     def self.add(item)
-#       items.insert(item.to_hash)
-#     end
+    def self.add(invoice_item)
+      invoice_items.insert(invoice_item.to_hash)
+    end
 
-#     def to_hash
-#       {
-#         id: id, name: name, description: description,
-#         unit_price: unit_price, merchant_id: merchant_id
-#       }
-#     end
+    def to_hash
+      {
+        id: id, item_id: item_id, invoice_id: invoice_id,
+        quantity: quantity, unit_price: unit_price
+      }
+    end
 
-#     def to_json(state = nil)
-#       {
-#         id: id, name: name, description: description,
-#         unit_price: unit_price, merchant_id: merchant_id
-#       }.to_json
-#     end
+   def to_json(state = nil)
+      {
+        id: id, item_id: item_id, invoice_id: invoice_id,
+        quantity: quantity, unit_price: unit_price
+      }.to_json
+    end
 
-#     def self.items
-#        Database.items
-#     end
+    def self.invoice_items
+       Database.invoice_items
+    end
 
-#     def self.find(id)
-#       result = items.limit(1).where(:id => id.to_i).first
-#       new(result) if result
-#     end
+    def self.find(id)
+      result = invoice_items.limit(1).where(:id => id.to_i).first
+      new(result) if result
+    end
 
-#     def self.find_by_name(name)
-#       result = items.limit(1).where(Sequel.ilike(:name, "%#{name}%")).first
-#       new(result) if result
-#     end
+    def self.find_by_invoice_id(invoice_id)
+      result = invoice_items.limit(1).where(invoice_id: invoice_id).first
+      new(result) if result
+    end
 
-#     def self.find_by_description(description)
-#       result = items.limit(1).where(Sequel.ilike(:description, "%#{description}%")).first
-#       new(result) if result
-#     end
-
-#     def self.find_by_merchant_id(merchant_id)
-#       result = items.limit(1).where(merchant_id: merchant_id).first
-#       new(result) if result
-#     end
-
-#     def self.find_all_by_name(name)
-#       results = items.where(Sequel.ilike(:name, "%#{name}")).to_a
-#       results.collect {|result| new(result)}
-#     end
-
-#     def self.find_all_by_description(description)
-#       results = items.where(Sequel.ilike(:description, "%#{description}")).to_a
-#       results.collect {|result| new(result)}
-#     end
-
-#     def self.random
-#       result = items.to_a.sample
-#       new(result) if result
-#     end
+    def self.random
+      result = invoice_items.to_a.sample
+      new(result) if result
+    end
   end
 end
--- spec/requests/invoice_items_spec.rb
+++ spec/requests/invoice_items_spec.rb
@@ -7,15 +7,25 @@ describe "/invoice_items/" do
     SalesEngineWeb::Server
   end
 
-  # let(:inv_item1) {SalesEngineWeb::InvoiceItem.create(item_id: item1.id, invoice_id: invoice1.id, quantity: 5, unit_price: 399)}
+  before(:each) do
+    customer1 && customer2 && merchant1 && invoice1 && invoice2 && item1 && inv_item1 && inv_item2
+  end
+  let(:customer1) {SalesEngineWeb::Customer.create(first_name: 'Clint', last_name: 'Eastwood')}
+  let(:customer2) {SalesEngineWeb::Customer.create(first_name: 'Whoopi', last_name: 'Goldberg')}
+  let(:merchant1) {SalesEngineWeb::Merchant.create(name: 'Hats N More')}
+  let(:invoice1) {SalesEngineWeb::Invoice.create(customer_id: customer1.id, merchant_id: merchant1.id)}
+  let(:invoice2) {SalesEngineWeb::Invoice.create(customer_id: customer2.id, merchant_id: merchant1.id)}
+  let(:item1) {SalesEngineWeb::Item.create(name: 'Shoes', merchant_id: merchant1.id, description: 'Blah blah', unit_price: 1999)}
+
+  let(:inv_item1) {SalesEngineWeb::InvoiceItem.create(item_id: item1.id, invoice_id: invoice1.id, quantity: 5, unit_price: 399)}
+  let(:inv_item2) {SalesEngineWeb::InvoiceItem.create(item_id: item1.id, invoice_id: invoice1.id, quantity: 90, unit_price: 199)}
 
   describe '/invoice_items/find' do
     context 'given an id' do
       it 'returns the invoice item with that id' do
-        pending
-       # get "invoice_items/find?id=#{inv_item1.id}"
-       # output = JSON.parse(last_response.body)
-       # expect(output['id']).to eq inv_item1.id
+       get "invoice_items/find?id=#{inv_item1.id}"
+       output = JSON.parse(last_response.body)
+       expect(output['id']).to eq inv_item1.id
       end
     end
 
@@ -73,7 +83,9 @@ describe "/invoice_items/" do
 
   describe '/invoice_items/random' do
     it 'returns a random invoice item instance' do
-      pending
+      get '/invoice_items/random'
+      output = JSON.parse(last_response.body)
+      expect( [ inv_item1.id, inv_item2.id ] ).to include( output['id'] )
     end
   end
 end
